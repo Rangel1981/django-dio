0382245306c45333f39f4651086ec089c14c54eb
Arthur <aarthur.rangel98@gmail.com>
2026-09-11 17:24:16 -0300

    comenaçando o mtv

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,6 +7,10 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     active = models.BooleanField(default=True)
 
+    class Meta:
+        verbose_name = "Questão"
+        verbose_name_plural = "Questões"
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - timezone.timedelta(days=1)
 
@@ -18,6 +22,10 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    class Meta:
+        verbose_name = "Opção"
+        verbose_name_plural = "Opções"
 
     def __str__(self):
         return self.choice_text
