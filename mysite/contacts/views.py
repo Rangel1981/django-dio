1fb5a4a8890189a42964ea38f0f6982ae5d2ee0d
Arthur <aarthur.rangel98@gamil.com>
2026-09-17 21:27:58 -0300

    aprendendo formulario

---
+++ mysite/contacts/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .forms import NameForm
+
+
+def get_name(request):
+    if request.method == "POST":
+        form = NameForm(request.POST)
+        
+        if form.is_valid():
+            name = form.cleaned_data["your_name"]
+            return HttpResponseRedirect(reverse("contacts:thanks", args=(name,)))
+
+
+    else:
+        form = NameForm()
+
+    return render(request, "contacts/name.html", {"form": form})
+
+
+def thanks(request, name):
+    return render(request, "contacts/thanks.html", {"name": name})
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin_site.urls),
     path("polls/", include("polls.urls")),
+    path("contacts/", include("contacts.urls")),
 ]
